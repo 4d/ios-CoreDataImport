987dc0da654c5ff8bc15927725ae17a15e905aa3
Eric Marchand <eric.marchand@4d.com>
2019-10-02 18:52:27 +0100

    [Mobile] coredataimport: add in test a checking about record count imported
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/CoreDataImport/": change = 242384]

--- test.sh
+++ test.sh
@@ -1,2 +1,33 @@
-./.build/release/coredataimport --structure Resources/Structures.xcdatamodeld --asset Resources/Assets.xcassets
-./.build/debug/coredataimport --structure Resources/Structures.xcdatamodeld --asset Resources/Assets.xcassets
+#/bin/bash
+
+mode="release" # release or debug
+./.build/$mode/coredataimport --structure Resources/Structures.xcdatamodeld --asset Resources/Assets.xcassets --output Resources
+
+# todo: make a loop on hash/dico
+table="EMPLOYES"
+expected=11
+count=$(sqlite3 Resources/Structures.sqlite "select count(*) FROM Z$table")
+if [ $count -eq $expected ]; then
+     echo "$count $table ok"
+ else
+     >&2 echo "expected $expected but receive $count for $table"
+     exit 1
+fi
+table="ALL_TYPES"
+expected=4
+count=$(sqlite3 Resources/Structures.sqlite "select count(*) FROM Z$table")
+if [ $count -eq $expected ]; then
+     echo "$count $table ok"
+ else
+     >&2 echo "expected $expected but receive $count for $table"
+     exit 1
+fi
+table="SERVICE"
+expected=0
+count=$(sqlite3 Resources/Structures.sqlite "select count(*) FROM Z$table")
+if [ $count -eq $expected ]; then
+     echo "$count $table ok"
+ else
+     >&2 echo "expected $expected but receive $count for $table"
+     exit 1
+fi
