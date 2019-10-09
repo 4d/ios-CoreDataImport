7596b6ff7cbed464ae6f7c6b79a7d27a56a2a9bf
Eric Marchand <eric.marchand@4d.com>
2019-10-09 13:31:59 +0100

    [Mobile] CoreDataDump: macOS graphical interface for this command #111171
    Jobs: redmine:110882
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/CoreDataImport/": change = 242721]

---
+++ CoreDataImportUI/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  CoreDataImportUI
+//
+//  Created by Eric Marchand on 09/10/2019.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
