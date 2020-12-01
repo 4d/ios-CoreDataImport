2c7c1da6672feb4bc76f5604d7d82662e337976a
Eric Marchand <eric.marchand@4d.com>
2020-12-01 17:08:49 +0100

    [Mobile] coredataimport: min version to 10.15 #122479
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/CoreDataImport/": change = 259018]

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "CoreDataImport",
     platforms: [
-        .macOS(.v10_14)
+        .macOS(.v10_15)
     ],
     products: [
         .executable(
